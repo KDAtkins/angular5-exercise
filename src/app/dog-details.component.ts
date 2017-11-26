d7e46be9994f6bcf35338e2bfc931b48069c7e17
Kevin Atkins <kaitoxas@gmail.com>
2017-11-26 13:03:23 -0700

    implemented FormsModule added 2-way binding to web application

--- src/app/dog-details.component.ts
+++ src/app/dog-details.component.ts
@@ -3,7 +3,8 @@ import { Component, Input, Output, EventEmitter } from '@angular/core';
 @Component({
    selector: 'dog-details',
    template: ' <h2>{{ name }} - {{ color }} </h2>' +
-   '<input (change) = "handleNameChange($event)" type="text"/>' +
+   '<input (input) = "handleNameChange($event)" type="text" placeholder="Name"/>' +
+   '<input (input) = "handleColorChange($event)" type="text" placeholder="Color"/>' +
    '<button class="btn btn-waves" (click) = "handleClick()">Bark</button>'
 })
 
@@ -13,6 +14,7 @@ export class DogDetailsComponent {
 
    @Output() bark = new EventEmitter<{}>();
    @Output() nameChange = new EventEmitter<string>();
+   @Output() colorChange = new EventEmitter<string>();
 
    handleClick() {
       this.bark.emit({});
@@ -20,5 +22,9 @@ export class DogDetailsComponent {
 
    handleNameChange(e) {
       this.nameChange.emit(e.target.value);
+   }
+
+   handleColorChange(e) {
+      this.colorChange.emit(e.target.value);
    }
 }
