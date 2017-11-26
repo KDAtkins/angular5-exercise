6262c031676d4bc18eca0acf4a24c143989fc131
Kevin Atkins <kaitoxas@gmail.com>
2017-11-26 11:35:27 -0700

    made dog-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
 import { DogDetailsComponent } from './dog-details.component';
+import { DogListComponent } from './dog-list/dog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DogDetailsComponent
+    DogDetailsComponent,
+    DogListComponent
   ],
   imports: [
     BrowserModule
