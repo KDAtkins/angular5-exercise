4312de32f1e1e8ddfef509a9ef55801050bbe868
Kevin Atkins <kaitoxas@gmail.com>
2017-11-26 11:45:44 -0700

    made dog-list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,28 +9,4 @@ export class AppComponent {
    title = 'Angular5 Exercise';
    dogGif = 'https://media.giphy.com/media/1LweXxLwVT0J2/giphy.gif';
    dogGifAlt = 'Golden Retriever';
-   dog1 = {name: 'Odie', color: 'Gold'};
-   dog2 = {name: 'Bane', color: 'Black'};
-   dog3 = {name: 'Daisy', color: 'Brown'};
-   dog4 = {name: 'Bane', color: 'Black'};
-
-   doBark(name: string) {
-      console.log(name + ' has barked.');
-   }
-
-   changeDog1(name) {
-      this.dog1.name = name;
-   }
-
-   changeDog2(name) {
-      this.dog2.name = name;
-   }
-
-   changeDog3(name) {
-      this.dog3.name = name;
-   }
-
-   changeDog4(name) {
-      this.dog4.name = name;
-   }
 }
---
+++ src/app/dog-list/dog-list.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'dog-list',
+  templateUrl: './dog-list.component.html',
+  styleUrls: ['./dog-list.component.css']
+})
+export class DogListComponent {
+   dog1 = {name: 'Odie', color: 'Gold'};
+   dog2 = {name: 'Bane', color: 'Black'};
+   dog3 = {name: 'Daisy', color: 'Brown'};
+   dog4 = {name: 'Bane', color: 'Black'};
+
+   doBark(name: string) {
+      console.log(name + ' has barked.');
+   }
+
+   changeDog1(name) {
+      this.dog1.name = name;
+   }
+
+   changeDog2(name) {
+      this.dog2.name = name;
+   }
+
+   changeDog3(name) {
+      this.dog3.name = name;
+   }
+
+   changeDog4(name) {
+      this.dog4.name = name;
+   }
+}
